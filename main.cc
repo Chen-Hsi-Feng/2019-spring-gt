6b6e0cebcacbf0da95ddf62926247ef7963cdda9
Chen-Hsi-Feng <a0986622528@gmail.com>
2019-06-10 10:13:13 +0800

    4 odd points finish adj update

--- main.cc
+++ main.cc
@@ -6,6 +6,7 @@
 #include "network_manager.h"
 #include "gplot.h"
 #include "path.h"
+#include <stdlib.h>
 
 using namespace std;
 
@@ -168,7 +169,7 @@ int main()
 int main(int argc, char** argv){
 
     /* start your program */
-    nm->interpret("./topo.txt");
+    nm->interpret("./topo_4points.txt");
 //    nm->print_all_e();
 //    nm->print_all_v();
     Vertex *list =  nm->get_all_nodes();
@@ -212,19 +213,21 @@ int array[size-1];
         edge=edge->next;
 }
 
-int odd[size-1];
+int odd[size];
+
 int odd_num = 0;
 
     for(int i = 0; i < size; i++)
     {
 	odd[i] = 0;
+	//record[i] = 1;
 }
 
     for(int i = 0; i < size; i++)
     {
 	cout << "array[" << i << "] : " << array[i] << endl;
 	
-	if ((array[i]/2)%2 != 0)
+	if ((array[i])%2 != 0)
 	{
 	odd[odd_num] = i;
 	
@@ -234,8 +237,11 @@ int odd_num = 0;
 	//cout << "odd_shit[" << odd_num-1 << "] : " << odd[odd_num-1] << endl;
 }
 
+cout << "odd_num : " << odd_num<<endl;
+
     for(int i = 0; i < odd_num; i++)
-    {
+    {	
+	
 	cout << "odd[" << i << "] : " << odd[i] << endl;
 
 }
@@ -279,14 +285,75 @@ int odd_num = 0;
 	cout << endl;
 }
 
-	
-	
-    int path[n] = {99, 99, 99, 99, 99, 99, 99, 99, 99, 99};
-	int length_true = 0;	
+	//----------------------add edges--------------------------//
 
+
+int record[odd_num];
+int record_points[odd_num];
 	
-	int start = odd[0];
-	int endline = odd[1]; 
+    for (int i = 0; i < odd_num; i++)
+    {
+	
+	record[i] = 1;
+	record_points[i] = 1;
+	cout << "record[ " << i << " ] : " << record[i] <<endl ;
+}
+
+int test = 0;
+    if (odd_num == 2)
+    test = 1;
+    else if (odd_num == 4)
+    test = 3;
+int cnt = 0;
+int length_true = 0;
+//int path[10];
+int start = 99;
+int endline = 99;
+int min_length_array[10] = {99, 99, 99, 99, 99, 99, 99, 99, 99, 99};
+int path[10] = {99, 99, 99, 99, 99, 99, 99, 99, 99, 99};
+while (test != 0)
+{
+    for (int i = 0; i < 10; i++)
+    path[i] = 99;
+
+    length_true = 0;	
+    int index_start = 0;
+    int index_end   = 0;
+    int start_done = 0;
+    int end_done = 0;
+    
+
+    while (start_done == 0)
+    {	
+	
+	if (record_points[index_start] == 1)
+	{
+	start = odd[index_start];
+	start_done = 1;
+}
+	else
+	index_start++;
+}
+	cout << "index_start : " << index_start << endl;
+	
+	cout << "start : " << start << endl;
+
+	index_end = index_start + 1;
+
+    while (end_done == 0)
+    {	
+	
+	if (record_points[index_end] == 1)
+	{
+	endline = odd[index_end];
+	end_done = 1;
+}
+	else
+	index_end++;
+}
+	record[index_end] = 0;
+	cout << "index_end : " << index_end << endl;
+	cout << "endline : " << endline << endl;
 
 	Dijkstra(adj, n, start, endline, path); 
 	
@@ -295,10 +362,8 @@ int odd_num = 0;
 	
 	for (int i = 0; i < n; i++)
 	{
-		
-		
-		if (path[i] != 99)
-		length_true++;
+   	    if (path[i] != 99)
+	    length_true++;
 				
 }
 	
@@ -310,17 +375,159 @@ int odd_num = 0;
 		cout << path[i] << " ";
 				
 }
-	cout << "length : " << length_true;
-  
+	cout << "length : " << length_true << endl;
+
+	min_length_array[cnt] = length_true;
 
 
+	test--;
+    
+    //record_points[index_start] = 0;
+    record_points[index_end] = 0;
+
+    for (int i = 0; i < odd_num; i++)
+    {
+	
+	cout << "record_points[ " << i << " ] : " << record_points[i] <<endl ;
+}
+	cnt++;
+	cout << "cnt : " << cnt << endl;
+}  // while end
+
+	int min = 99;
+        int min_index = 0;
+	int start1 = 99;
+	int endline1 = 99;
+	int start2 = 99;
+	int endline2 = 99;
+	
+	for (int i = 0; i < cnt; i++)
+	{
+	cout << "min_length_array[" << i << "] : " << min_length_array[i] <<endl;
+	if (min_length_array[i] - min < min)
+	{
+	min = min_length_array[i];
+	min_index = i;
+}
+}
+
+	cout << "min : " << min << endl;
+	cout << "min_index : " << min_index << endl;
+
+
+	if (min_index == 0)	
+	{
+	start1 = odd[0];
+	endline1 = odd[1];
+	start2 = odd[2];
+	endline2 = odd[3];
+	//cout << "min_index : " << min_index << endl;	
+}
+	
+	else if (min_index == 1)	
+	{
+	start1 = odd[0];
+	endline1 = odd[2];
+	start2 = odd[1];
+	endline2 = odd[3];
+	//cout << "min_index : " << min_index << endl;	
+	//fgetc(stdin);   //pause
+
+}	
+
+	else if (min_index == 2)	
+	{
+	start1 = odd[0];
+	endline1 = odd[3];	
+	start2 = odd[1];
+	endline2 = odd[2];
+	//cout << "min_index : " << min_index << endl;
+}	
+
+	cout << "start1 : " << start1 <<endl;
+	cout << "endline1 : " << endline1 <<endl;
+	cout << "start2 : " << start2 <<endl;
+	cout << "endline2 : " << endline2 <<endl;
+
+fgetc(stdin);   //pause
+
+    if (odd_num == 4)
+    {
+	int times = 2;
+	while (times != 0)
+	{
+
+    for (int i = 0; i < 10; i++)
+    path[i] = 99;
+
+    length_true = 0;
+
+
+	if (times == 2)
+	{
+	start = start1;
+	endline = endline1;
+}
+
+	else if (times == 1)
+	{
+	start = start2;
+	endline = endline2;
+}
+
+	Dijkstra(adj, n, start, endline, path); 
+	
+	cout << endl;
+	cout << "path : ";
+	
+	for (int i = 0; i < n; i++)
+	{
+   	    if (path[i] != 99)
+	    length_true++;
+				
+}
+	
+	
+	path[length_true] = start;
+	
+	for (int i = 0; i < n; i++)
+	{
+		cout << path[i] << " ";
+				
+}
+	cout << "length : " << length_true << endl;
+
+
+	for (int i =  0; i < length_true; i++)  // update adj matrix
+    {
+	adj[path[i]][path[i+1]]++;
+	adj[path[i+1]][path[i]]++;
+	
+}
+
+cout << endl;
+
+    cout << "adj matrix update : " << endl;    // adj. matrix
+    for(int i = 0; i < 10; i++)    
+    {
+	for(int j = 0; j < 10; j++)
+	{
+	cout << adj[i][j] << " ";
+}
+	cout << endl;
+}
+	times--;
+}   // end while
+}    // end if 
+
+
+else if (odd_num = 2)
+{
     for (int i =  0; i < length_true; i++)  // update adj matrix
     {
-	for (int j = 0; j < length_true; j++)
-	{
-	adj[start][endline]++;
-	adj[endline][start]++;
-}	
+	adj[path[i]][path[i+1]]++;
+	adj[path[i+1]][path[i]]++;
+	
 }
 
 cout << endl;
@@ -335,7 +542,7 @@ cout << endl;
 	cout << endl;
 }
 	
-
+}
 
 	//-------------------------path--------------------------//
 
